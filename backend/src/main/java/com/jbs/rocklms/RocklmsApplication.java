131de1b10bee539dd4d37b25bbba2ffa4aadcdee
Jakub Brylak <brylakjakub@gmail.com>
2025-07-13 13:53:39 +0200

    Initial spring boot project

---
+++ backend/src/main/java/com/jbs/rocklms/RocklmsApplication.java
@@ -0,0 +1,13 @@
+package com.jbs.rocklms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocklmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocklmsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jbs/rocklms/RocklmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbs.rocklms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocklmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
