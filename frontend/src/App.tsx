cf80ecc3aa32279977d2c3e23d11ad7930cec81a
Jakub Brylak <brylakjakub@gmail.com>
2025-07-14 01:45:01 +0200

    Fetching Courses using generated api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import { CoursesApi } from './api/apis/CoursesApi'
+import type { CourseDto } from './api/models/CourseDto'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [courses, setCourses] = useState<CourseDto[]>([])
+
+  useEffect(() => {
+    new CoursesApi().findAllCourses()
+      .then(coursesResult => setCourses(coursesResult))
+  }, [])
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Rock LMS Courses</h1>
+        <ol>
+          {courses.map(course => (
+            <span key = {course.id}>
+              <li >{course.title}</li>
+              <ul>
+                <li>Description: {course.description}</li>
+                <li>Duration: {course.duration}</li>
+                <li>Status: {course.status}</li>
+              </ul>
+            </span>
+          ))}
+        </ol>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
